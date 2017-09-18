0b5ed6a428a8426f6c54e7ca0beee633919ff919
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-09-19 00:38:01 +0300

    delete error from test

--- src/test/java/RunGoogle/TestGoogleRun.java
+++ src/test/java/RunGoogle/TestGoogleRun.java
@@ -30,12 +30,4 @@ public class TestGoogleRun extends TestConfig{
         System.out.println(title);
         Assert.assertTrue(title.equals(expected));
     }
-
-    @Test
-    public void testGoogleRun3(){
-        String expected = "Google";
-        title = driver.getTitle();
-        System.out.println(title);
-        Assert.assertTrue(title.equals(expected));
-    }
 }
