e03ba98a9e9e234cd20bf7cad7f4028000f3cd21
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-09-21 02:13:17 +0300

    add loggger final version

--- src/test/java/RunGoogle/TestConfig.java
+++ src/test/java/RunGoogle/TestConfig.java
@@ -3,23 +3,53 @@ package RunGoogle;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatchman;
+import org.junit.runners.model.FrameworkMethod;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
 
 public class TestConfig{
-    public WebDriver driver;
+    WebDriver driver;
     private GetWebDriver automation;
+    private Handler fh;
+    private Logger log = Logger.getLogger(TestCase.class.getName());
+    @Rule
+    @SuppressWarnings("deprecation")
+    public TestWatchman watchman = new TestWatchman() {
+        public void succeeded(FrameworkMethod method) {
+            log.info("PASS, METHOD: " + method.getName());
+        }
+
+        public void failed(Throwable e, FrameworkMethod method) {
+            StringWriter stack = new StringWriter();
+            e.printStackTrace(new PrintWriter(stack));
+            log.severe("FAIL, METHOD: " + method.getName() + "\r\n"
+                    + "      REASON: " + stack.toString());
+        }
+    };
+
+    @After
+    public void quit() {
+        driver.quit();
+    }
 
     @Before
-    public void createDriver() throws InterruptedException {
+    public void createDriver() throws InterruptedException, IOException {
+        fh = new FileHandler("./log/myLog.log");
+        //fh.setFilter(new MyFilter());
+        fh.setFormatter(new MyFormatter());
+        log.setUseParentHandlers(false);
+        log.addHandler(fh);
         automation = GetWebDriver.getInstance();
         driver = automation.openBrowser();
         driver.get("http://www.google.com");
         Thread.sleep(1000);
-    }
-
-    @After
-    public void quit(){
-        driver.quit();
     }
 }
